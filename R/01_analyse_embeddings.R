898923ce6b1832bc7dde93e758d38da05e83f801
tbgitoo <thomas.braschler@gmail.com>
2025-11-30 19:14:12 +0100

    starting decoder work

--- R/01_analyse_embeddings.R
+++ R/01_analyse_embeddings.R
@@ -6,5 +6,5 @@ for_umap=embeddings_data[,4:ncol(embeddings_data)]
 
 umap=umap(for_umap)
 
-plot(umap$layout[1:1000,], pch=20, 
-col=as.integer(as.factor(embeddings_data$track_id[1:1000]))+1)
+plot(umap$layout, pch=20, 
+col=as.integer(as.factor(embeddings_data$track_id))+1)
